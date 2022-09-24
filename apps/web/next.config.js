780ceeff855f306ac41c50a337845b231176ea7c
Baily Troyer <bailytroyer@gmail.com>
2022-09-24 17:46:40 -0400

    add bundle analyzer

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,6 +5,10 @@
 
 const { withSentryConfig } = require("@sentry/nextjs");
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const moduleExports = {
   reactStrictMode: true,
 
@@ -33,4 +37,6 @@ const sentryWebpackPluginOptions = {
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withBundleAnalyzer(
+  withSentryConfig(moduleExports, sentryWebpackPluginOptions)
+);
