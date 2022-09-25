9f77e9be8730b9caee3609d8ad540b4942e7ae42
Baily Troyer <bailytroyer@gmail.com>
2022-09-25 19:06:31 -0400

    fix path for next auth

--- apps/web/src/pages/api/auth/[...nextauth].ts
+++ apps/web/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
 
-import { authOptions } from "../../../../common/auth";
+import { authOptions } from "../../../common/auth";
 
 export default NextAuth(authOptions);
