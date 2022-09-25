bcc4794c63edac6b45a1b895c5d8d781674b3c88
Baily Troyer <bailytroyer@gmail.com>
2022-09-25 19:34:51 -0400

    use NEXT_PUBLIC_VERCEL_URL instead of VERCEL_URL

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -40,8 +40,8 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
 
 export default withTRPC<ServerRouter>({
   config() {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return {
